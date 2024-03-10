c011b81ece5ef526973645b0addd87eab418b45d
shovanpaul48 <shovanpaul48@gmail.com>
2024-03-10 19:26:50 +0530

    Add unit test for index view

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from django.urls import reverse
+from .views import index
+
+class TestIndexView(TestCase):
+    def test_index_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello, Django! From Shovan")
