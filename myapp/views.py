705648bc7162fe0a9c6e9a7fb2db0d4b1a2c630b
shovanpaul48 <shovanpaul48@gmail.com>
2024-03-10 19:34:51 +0530

    Error added

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+dhgj
 def index(request):
-    return HttpResponse("Hello, Django!")
+    return HttpResponse("Hello, Django!" 
+                        d;lkl
 
