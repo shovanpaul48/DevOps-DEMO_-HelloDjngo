ff86ee1bbfa8cbbafbae5f095a2287f353dd4e14
shovanpaul48 <shovanpaul48@gmail.com>
2024-03-10 19:27:58 +0530

    Error Added

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, Django! From Shovan)
+    return HttpResponse("Hello, Django! From Shovan"
